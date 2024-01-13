621026c24ce2020d34b81fd44aee4e22cf711022
Valeriy Danets <dantesvv@gmail.com>
2024-01-13 18:31:35 +0200

    adding Navbar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import Menu from "./Menu";
+
+interface NavbarProps {
+  currentUser?: null;
+}
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+        <Container>
+          <div
+            className="
+            flex 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+          >
+            <Logo />
+            <Search />
+            <Menu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
